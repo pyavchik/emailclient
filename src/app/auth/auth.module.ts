a6c6eb97fe29a13c2af2b72c3a2d213426baafff
pyavchik <pyavchik@gmail.com>
2020-03-16 23:15:41 +0200

    routes added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import {Routes} from '@angular/router';
 
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: SigninComponent }
+];
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
