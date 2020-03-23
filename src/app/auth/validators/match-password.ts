a99e7944e660cff60ff78b66f1a90d83317ced66
pyavchik <pyavchik@gmail.com>
2020-03-23 21:19:50 +0200

    custom validator added

---
+++ src/app/auth/validators/match-password.ts
@@ -0,0 +1,12 @@
+import {AbstractControl, FormGroup, ValidationErrors, Validator} from '@angular/forms';
+
+export class MatchPassword implements Validator {
+  validate(formGroup: FormGroup): ValidationErrors | null {
+    const {password, passwordConfirmation} = formGroup.value;
+    if (password === passwordConfirmation) {
+      return null;
+    } else {
+      return {passwordsDontMatch: true};
+    }
+  }
+}
